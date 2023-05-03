60fafd86490494d88fb8fe1e997f749ff146a2a2
RubenWihler <wihlerruben@gmail.com>
2023-05-03 15:56:43 +0200

    added connexion system

---
+++ Js/Connexion/connexion_manager.js
@@ -0,0 +1,52 @@
+class ConnexionManager {
+    static #instance = null;
+    #connected = false;
+    #token = null;
+    #email = null;
+    constructor() {
+        ConnexionManager.#instance = this;
+    }
+    static get instance() {
+        if (ConnexionManager.#instance === null) {
+            new ConnexionManager();
+        }
+        return ConnexionManager.#instance;
+    }
+    static get connected() {
+        return ConnexionManager.instance.#connected;
+    }
+    static get token() {
+        return ConnexionManager.instance.#token;
+    }
+    static get email() {
+        return ConnexionManager.instance.#email;
+    }
+    static connect(email, token, saveToLocalStorage = false) {
+        ConnexionManager.instance.#email = email;
+        ConnexionManager.instance.#token = token;
+        ConnexionManager.instance.#connected = true;
+        console.log('connected');
+        if (saveToLocalStorage)
+            this.#saveToLocalStorage(email, token);
+    }
+    static tryLoginFromLocalStorage() {
+        let result = this.#getFromLocalStorage();
+        if (!result.founded)
+            return false;
+        this.connect(result.result.email, result.result.token);
+        return true;
+    }
+    static #saveToLocalStorage(email, token) {
+        let object = {
+            email: email,
+            token: token
+        };
+        localStorage.setItem('connection_token', JSON.stringify(object));
+    }
+    static #getFromLocalStorage() {
+        let result = localStorage.getItem('connection_token');
+        if (result === null)
+            return { founded: false, result: null };
+        return { founded: true, result: JSON.parse(result) };
+    }
+}
