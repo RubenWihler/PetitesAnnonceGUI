0669b367c7470905a34178596730931b5fe970c0
RubenWihler <wihlerruben@gmail.com>
2023-05-03 15:56:33 +0200

    added connexion system

--- Js/Processors/connexion_manager.js
+++ Js/Processors/connexion_manager.js
@@ -2,4 +2,11 @@ class ConnexionManager {
     static get apiBaseUrl() {
         return 'https://michaelmathieu.net/smallAds/';
     }
+    static get accountToken() {
+        //gerer la connexion si l'utilisateur est connecté et tout ca la
+        return {
+            isConnected: false,
+            token: null
+        };
+    }
 }
